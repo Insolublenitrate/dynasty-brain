895a0840dd3cae650143af9287b2ab9173634885
ISO <insolublenitrate@gmail.com>
2026-06-20 17:39:36 -0400

    Add Capacitor Android project, configuration, and app icon

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.athleticintel.wiretap',
+  appName: 'WaiverWireTap',
+  webDir: 'out'
+};
+
+export default config;
